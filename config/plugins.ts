d9e74cc548b12344a4c3a9085eacc7392d021f69
leo.yao <leo.yau@tecky.io>
2023-09-11 15:37:20 +0800

    added/coverimagePreview

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,21 @@
 module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.gmail.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: env('DEFAULT_EMAIL'),
+        defaultReplyTo: env('DEFAULT_EMAIL'),
+      },
+    },
+  },
   upload: {
     config: {
       provider: 'aws-s3',
